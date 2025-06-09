8d1dc1441a7ae489157bb8d3fc2d0ce7bf0650dd
rafiammar014 <rafiammar014@gmail.com>
2025-06-09 10:33:53 +0700

    i blok catch untuk penanganan exception bertipe int

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -8,4 +8,8 @@ int main()
         throw 0.5; // melemparkan sebuah integer maka 
         cout << "Pernyataan itidak akan dieksekusi" << endl;
     }
+    catch (int a) {
+        //blok ini akan dieksekusi 
+        cout << "pengecualian akan dieksekusi" << endl;
+    }
 }
