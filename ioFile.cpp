8d9bb22051ac7a8675079e64dd8035296630ba3f
rafiammar014 <rafiammar014@gmail.com>
2025-06-09 10:01:36 +0700

    membuat main baris

---
+++ ioFile.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+
+    // operasi file dalam mode menulis.
+    ofstream outfiles;
+    // menunjuk ke sebuah nama file untuk membuka 
+    outfile.open("contoh.txt");
+
+    cout << ">= Menulis file, \'q'\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis 
+    while (true) {
+        cout << "_ ";
+        //mendapatkan setiap karakter dalam satu baris 
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q 
+        if (baris == "q") break;
+    }
+}
