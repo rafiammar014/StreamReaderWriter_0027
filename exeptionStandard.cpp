198db9c8cf7bd47cf11e592651380a010400ddee
rafiammar014 <rafiammar014@gmail.com>
2025-06-09 10:57:03 +0700

    commit

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <exeption>
+#include <exception> // perbaiki typo dari <exeption> ke <exception>
 //untuk obyek exception yang akan digunakan
 #include <array>
 //untuk obyek array yang akan kita gunakan
@@ -11,16 +11,16 @@ int main()
     try {
         array<int, 3> data = { 1, 2, 3 }; 
         //pesan array integer 3 elemen 
-        cout<<data.at(5)<<endl;
+        cout << data.at(5) << endl;
         // memanggil array elemen ke 5
     }
-    catch (exeception& e) {
-        //penangkap menggunakan obyek exeception
+    catch (exception& e) { // perbaiki typo exeception -> exception
+        //penangkap menggunakan obyek exception
         cout << e.what() << endl;
         /*akan dieksekusi karna array data hanya memiliki 3 elemen */
     }
     cout << "Baris Program yang Terakhir" << endl;
     /*penanda 2" bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
 
-    return 0; 
+    return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+    cout << "Baris Program yang Terakhir" << endl;
+    /*penanda 2" bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+
+    return 0; 
