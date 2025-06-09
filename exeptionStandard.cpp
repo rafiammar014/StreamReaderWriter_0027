fe1d00d7d6e5448182272dd811fcddf6d498a237
rafiammar014 <rafiammar014@gmail.com>
2025-06-09 11:08:29 +0700

    perbaikan

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <exception> // perbaiki typo dari <exeption> ke <exception>
+#include <exception> 
 //untuk obyek exception yang akan digunakan
 #include <array>
 //untuk obyek array yang akan kita gunakan
